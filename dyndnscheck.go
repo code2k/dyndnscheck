d4ea84dcd4f519fbeeb8e772c6cc4981f4d50191
Andreas Pataki <pataki.andreas@googlemail.com>
2014-04-14 16:52:34 +0200

    added config command line flag better error handling better alert email

--- dyndnscheck.go
+++ dyndnscheck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -11,6 +12,10 @@ import (
 	"os"
 	"strconv"
 	"strings"
+)
+
+const (
+	VERSION string = "1.0"
 )
 
 type Config struct {
@@ -24,7 +29,9 @@ type Config struct {
 	EMailPassword string
 }
 
-var config = &Config{}
+var (
+	config = &Config{}
+)
 
 func loadConfig(filename string) {
 	file, err := ioutil.ReadFile(filename)
@@ -56,67 +63,89 @@ func sendMail(subject string, message string) {
 
 }
 
+// getDynIP returns the current IP of your DnyDNS host.
 func getDynIP(host string) (ip string, err error) {
 	ips, err := net.LookupIP(host)
 	if err != nil {
 		log.Printf("getDynIP: %v", err)
-	} else {
-		ip = ips[0].String()
+		return
 	}
+	ip = ips[0].String()
+	log.Printf("DynDns IP: %v", ip)
 	return
 }
 
+// getCurrentIP returns your external IP by using a web service
+// like http://ifconfig.me/ip or http://icanhazip.com.
 func getCurrentIP() (ip string, err error) {
 	res, err := http.Get(config.CurrentIPURL)
 	if err != nil {
-		log.Printf("Error: %v", err)
-	} else if res.StatusCode != http.StatusOK {
-		err = fmt.Errorf("Can't get IP from %s: %s", config.CurrentIPURL, res.Status)
-	} else {
-		body, err := ioutil.ReadAll(res.Body)
-		res.Body.Close()
-		if err != nil {
-			log.Printf("Error: %v", err)
-		} else {
-			ip = strings.TrimSpace(string(body))
-		}
+		log.Printf("getCurrentIP: %v", err)
+		return
 	}
+	if res.StatusCode != http.StatusOK {
+		err = fmt.Errorf("Can't get IP from %s: %s", config.CurrentIPURL, res.Status)
+		return
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Printf("getCurrentIP: %v", err)
+		return
+	}
+	ip = strings.TrimSpace(string(body))
+	if net.ParseIP(ip) == nil {
+		log.Printf("Result is not an IP address: %s", ip)
+		ip = ""
+		err = fmt.Errorf("%s does not returns an IP address!", config.CurrentIPURL)
+	}
+	log.Printf("Current IP: %v", ip)
 	return
 }
 
-func main() {
-	loadConfig("config.json")
+func init() {
+	config := flag.String("config", "config.json", "location of the configuration file")
+	flag.Parse()
 
+	log.Print("DynDNSCheck " + VERSION)
+	log.Print("Configuration: " + *config)
+
+	loadConfig(*config)
+}
+
+func main() {
 	currentIP, cerr := getCurrentIP()
 	dynIP, derr := getDynIP(config.DynDNSHost)
 
-	var message string
+	if cerr != nil || derr != nil || currentIP != dynIP {
+		// Something is wrong. Generate alert email:
+		var message string
 
-	if cerr != nil {
-		message += fmt.Sprintf("Error getting current IP: %v\n", cerr)
-	} else {
-		log.Printf("Current IP: %v", currentIP)
-	}
-	if derr != nil {
-		message += fmt.Sprintf("Error getting DynDNS IP: %v\n", derr)
-	} else {
-		log.Printf("DynDns IP: %v", dynIP)
-	}
-	if currentIP != dynIP && len(message) == 0 {
-		message += fmt.Sprintf("IPs are different!\n\nCurrent IP: %s\nDynDNS IP:%s\n", currentIP, dynIP)
-	}
+		if cerr != nil {
+			message += fmt.Sprintf("Error getting current IP: %v\n", cerr)
+		}
+		if derr != nil {
+			message += fmt.Sprintf("Error getting DynDNS IP: %v\n", derr)
+		}
+		if currentIP != dynIP && len(message) == 0 {
+			message += fmt.Sprintf("IPs are different!\n\nDynDNS IP:%s\n", dynIP)
+		}
+		if cerr == nil {
+			// if available always append the current IP
+			message += fmt.Sprintf("Current IP: %s\n", currentIP)
+		}
 
-	if len(message) > 0 {
 		message += fmt.Sprintf("\nChecked Host: %s\n", config.DynDNSHost)
 
 		hostname, err := os.Hostname()
 		if err != nil {
 			hostname = fmt.Sprintf("Error: %v", err)
 		}
-		message += "DynDNSCheck is running on host " + hostname
+		message += "DynDNSCheck " + VERSION + " is running on " + hostname
 
 		log.Print("Check failed... sending alert")
 		sendMail(config.EMailSubject, message)
+
 	} else {
 		log.Print("Check OK")
 	}
